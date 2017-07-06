f7fc4eef4943b2e13c5c60e5c8419bfa9e00a270
Tanvi Kumar <tanvimatrix@gmail.com>
2017-07-06 23:08:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,51 +5,60 @@
 		<title>Welcome Home</title>
 		<link rel="stylesheet" href="style.css" />
 		<?php
-		include("auth.php");
-		require('db.php');
-		if(isset($_REQUEST['submit'])){
-			
+			include("auth.php");
+			require('db.php');
+			if(isset($_REQUEST['submit'])){
+				
 
-			if(isset($_POST["title"])){
-				$title = $_POST["title"];
-			}
+				if(isset($_POST["title"])){
+					$title = $_POST["title"];
+				}
 
-			if(isset($_POST["snippet"])){
-				$snippet = $_POST["snippet"];
-			}
-			
-			$username = $_SESSION['username'];
+				if(isset($_POST["snippet"])){
+					$snippet = $_POST["snippet"];
+				}
+				
+				$username = $_SESSION['username'];
 
-			if(isset($_POST["privacy"])){
-				$privacy=1;
-			}
-			else{
-				$privacy=0;
-			}
+				if(isset($_POST["privacy"])&&$_POST["privacy"]=="private"){
+					$privacy=1;
+				}
+				else{
+					$privacy=0;
+				}
 
-			if(isset($_POST["language"])){
-				$language=$_POST["language"];
+				if(isset($_POST["language"])){
+					$language=$_POST["language"];
 
-			}
+				}
 
-			if(isset($_POST["anonymity"])){
-				$anonymity=1;
-			}
-			else{
-				$anonymity=0;
-			}
+				if(isset($_POST["anonymity"])&&$_POST["anonymity"]=="unknown"){
+					$anonymity=1;
+				}
+				else{
+					$anonymity=0;
+				}
 
-            if($stmt= $con->prepare("INSERT into `snippets` (`username`, `title`, `snippet`, `language`, `privacy`, `anonimity`) VALUES (?,?,?,?,?,?)")){
+	            if($stmt= $con->prepare("INSERT into `snippets` (`username`, `title`, `snippet`, `language`, `privacy`, `anonimity`) VALUES (?,?,?,?,?,?)")){
                     $stmt->bind_param("ssssii", $username, $title, $snippet, $language,$privacy,$anonymity);
                     $stmt->execute();
                     $stmt->close();
-                }
-            else{
-              	echo "fail";
-              }
-              
-		}else{
-		};
+                    //echo "Successfully inserted. ";
+                    //echo "<br> <br>";
+                    $query_display = "SELECT id,title FROM snippets ORDER BY id DESC LIMIT 1";
+		        	$sql = mysqli_query($con,$query_display);
+	        		while($rows = mysqli_fetch_array($sql)){
+		        		header("Location: snippet.php?id=".$rows['id']); 
+	                }
+	            }
+	            else{
+	              	echo "Failure in inserting.";
+	             }
+
+		       exit;
+
+			}else{
+			};
 		?>
 	</head>
 	<body>
@@ -62,9 +71,10 @@
 			<textarea rows="25" cols="50" name="snippet" placeholder=" Enter Snippet"></textarea>
 			<br>
 			<br>
-			<p> Keep it Private : <input type = "radio" name = "privacy"></p>
-      		<p> Anonymous : <input type = "radio" name = "anonymity"></p>	
-
+			<p> Keep it Private : <input type = "radio" name = "privacy" value="private"></p>
+			<p> Keep it Public : <input type = "radio" name = "privacy" value="public" checked></p>
+      		<p> Anonymous : <input type = "radio" name = "anonymity" value="unknown"></p>	
+      		<p> Not Anonymous : <input type = "radio" name = "anonymity" value="shown" checked></p>
 			<input type="text" name="language" placeholder="language" required>
 			<br>
 			<br>
